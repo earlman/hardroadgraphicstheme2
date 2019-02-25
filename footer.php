c2634ae9aba4ccf786c7c500230847005c85c1cb
earlman <earl.mangulabnan@gmail.com>
2019-02-25 05:58:55 -0600

    imported templates from first version

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package Hardroad_Graphics
+ * @package hardroad1
  */
 
 ?>
@@ -14,18 +14,35 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
+
+
+
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'hardroadgraphicstheme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'hardroadgraphicstheme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'hardroadgraphicstheme' ), 'hardroadgraphicstheme', '<a href="http://underscores.me/">Earl Mangulabnan</a>' );
-				?>
+            <div id="footer-sidebar" class="secondary">
+                <div id="footer-sidebar1">
+                    <?php
+                    if(is_active_sidebar('footer-sidebar-1')){
+                    dynamic_sidebar('footer-sidebar-1');
+                    }
+                    ?>
+                </div>
+                <div id="footer-sidebar2">
+                    <?php
+                    if(is_active_sidebar('footer-sidebar-2')){
+                    dynamic_sidebar('footer-sidebar-2');
+                    }
+                    ?>
+                </div>
+                <div id="footer-sidebar3">
+                    <?php
+                    if(is_active_sidebar('footer-sidebar-3')){
+                    dynamic_sidebar('footer-sidebar-3');
+                    }
+                    ?>
+                </div>
+            </div>
+                
+
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
---
+++ front-page.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package hardroad1
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		<?php
+            $pages = get_pages(array('sort_column' => 'menu_order')); 
+            foreach ($pages as $page_data) {
+                $content = apply_filters('the_content', $page_data->post_content); 
+                $title = $page_data->post_title; 
+                echo "<h1 class=\"page-title\">", $title, "</h1>" ;
+
+                echo $content; 
+                
+            }
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+//get_sidebar();
+get_footer();
