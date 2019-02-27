88b62683c775707f65b65b5c09561bad6231f8f9
earlman <earl.mangulabnan@gmail.com>
2019-02-26 22:37:33 -0600

    styles.sass to styles.scss

--- wpgulp.config.js
+++ wpgulp.config.js
@@ -16,7 +16,7 @@ module.exports = {
 	injectChanges: true,
 
 	// Style options.
-	styleSRC: './assets/css/style.sass', // Path to main .scss file.
+	styleSRC: './assets/css/style.scss', // Path to main .scss file.
 	styleDestination: './', // Path to place the compiled CSS file. Default set to root folder.
 	outputStyle: 'expanded', // Available options → 'compact' or 'compressed' or 'nested' or 'expanded'
 	errLogToConsole: true,
@@ -37,7 +37,7 @@ module.exports = {
 	imgDST: './assets/img/', // Destination folder of optimized images. Must be different from the imagesSRC folder.
 
 	// Watch files paths.
-	watchStyles: [ './assets/css/**/*.sass', './assets/css/**/*.sass' ], // Path to all *.scss files inside css folder and inside them.
+	watchStyles: [ './assets/css/**/*.scss' ], // Path to all *.scss files inside css folder and inside them.
 	watchJsVendor: './assets/js/vendor/*.js', // Path to all vendor JS files.
 	watchJsCustom: './assets/js/custom/*.js', // Path to all custom JS files.
 	watchPhp: './**/*.php', // Path to all PHP files.
